752981cad1e43415047a43aa40143f4e19fd2f80
hoondeveloper <hoonskyn@cau.ac.kr>
2021-09-02 23:05:57 +0900

    feat: hoondev 색상 상수 구현, TS에서 import해서 사용할 수 있도록 ts 정의부 구현

---
+++ shared/constants/colors.d.ts
@@ -0,0 +1,75 @@
+export const COLORS: {
+  current: string;
+  transparent: string;
+  white: string;
+  black: string;
+  primary: {
+    DEFAULT: string;
+    darker: string;
+    lighter: string;
+    subtle: string;
+  };
+  dark: {
+    0: string;
+    1: string;
+    2: string;
+    3: string;
+    4: string;
+  };
+  light: {
+    0: string;
+    1: string;
+    2: string;
+    3: string;
+    4: string;
+  };
+  red: {
+    0: string;
+    1: string;
+    2: string;
+    3: string;
+    4: string;
+  };
+  green: {
+    0: string;
+    1: string;
+    2: string;
+    3: string;
+    4: string;
+  };
+  blue: {
+    0: string;
+    1: string;
+    2: string;
+    3: string;
+    4: string;
+  };
+  yellow: {
+    0: string;
+    1: string;
+    2: string;
+    3: string;
+    4: string;
+  };
+  orange: {
+    0: string;
+    1: string;
+    2: string;
+    3: string;
+    4: string;
+  };
+  teal: {
+    0: string;
+    1: string;
+    2: string;
+    3: string;
+    4: string;
+  };
+  purple: {
+    0: string;
+    1: string;
+    2: string;
+    3: string;
+    4: string;
+  };
+};
---
+++ shared/constants/tailwindColors.js
@@ -0,0 +1,75 @@
+module.exports = {
+  current: 'currentColor',
+  transparent: 'transparent',
+  white: '#FFFFFF',
+  black: '#000000',
+  primary: {
+    DEFAULT: '#3e7bfa',
+    darker: '#3568d4',
+    lighter: '#6698fa',
+    subtle: '#ccddff'
+  },
+  dark: {
+    0: '#1c1c28',
+    1: '#28293d',
+    2: '#555770',
+    3: '#8f90a6',
+    4: '#c7c9d9'
+  },
+  light: {
+    0: '#e4e4eb',
+    1: '#ebebf0',
+    2: '#f2f2f5',
+    3: '#fafafc',
+    4: '#ffffff'
+  },
+  red: {
+    0: '#e53535',
+    1: '#ff3b3b',
+    2: '#ff5c5c',
+    3: '#ff8080',
+    4: '#ffe5e5'
+  },
+  green: {
+    0: '#05a660',
+    1: '#06c270',
+    2: '#39d98a',
+    3: '#57eba1',
+    4: '#e3fff1'
+  },
+  blue: {
+    0: '#004fc4',
+    1: '#0063f7',
+    2: '#5b8def',
+    3: '#9dbff9',
+    4: '#e5f0ff'
+  },
+  yellow: {
+    0: '#e5b800',
+    1: '#ffcc00',
+    2: '#fddd48',
+    3: '#fded72',
+    4: '#fffee5'
+  },
+  orange: {
+    0: '#e57a00',
+    1: '#ff8800',
+    2: '#fdac42',
+    3: '#fccc75',
+    4: '#fff8e5'
+  },
+  teal: {
+    0: '#00b7c4',
+    1: '#00cfde',
+    2: '#73dfe7',
+    3: '#a9eff2',
+    4: '#e5ffff'
+  },
+  purple: {
+    0: '#4d0099',
+    1: '#6600cc',
+    2: '#ac5dd9',
+    3: '#dda5e9',
+    4: '#ffe5ff'
+  }
+};
