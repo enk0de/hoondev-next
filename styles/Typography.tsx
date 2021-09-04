d68dc08dd8e7339d2be9f125f0b71f9913b91d83
hoondeveloper <hoonskyn@cau.ac.kr>
2021-09-04 21:25:32 +0900

    feat: Typography styled 구현

---
+++ styles/Typography.tsx
@@ -0,0 +1,142 @@
+import styled from '@emotion/styled';
+
+export const TypoSmallLabel = styled.span`
+  font-size: 10px;
+  font-weight: bold;
+  font-style: normal;
+  line-height: 14;
+  letter-spacing: 0.2px;
+  text-decoration: none;
+`;
+
+export const TypoMediumLabel = styled.span`
+  font-size: 12px;
+  font-weight: bold;
+  font-style: normal;
+  line-height: 16;
+  letter-spacing: 0.2px;
+  text-decoration: none;
+`;
+
+export const TypoLargeLabel = styled.span`
+  font-size: 14px;
+  font-weight: bold;
+  font-style: normal;
+  line-height: 24;
+  text-decoration: none;
+`;
+
+export const TypoBody16 = styled.p`
+  font-size: 16px;
+  font-weight: normal;
+  font-style: normal;
+  line-height: 24;
+  text-decoration: none;
+`;
+
+export const TypoBody14 = styled.p`
+  font-size: 14px;
+  font-weight: normal;
+  font-style: normal;
+  line-height: 24;
+  text-decoration: none;
+`;
+
+export const TypoH1 = styled.h1`
+  font-size: 40px;
+  font-weight: bolder;
+  font-style: normal;
+  line-height: 54;
+  text-decoration: none;
+`;
+
+export const TypoH2 = styled.h2`
+  font-size: 32px;
+  font-weight: bolder;
+  font-style: normal;
+  line-height: 44;
+  text-decoration: none;
+`;
+
+export const TypoH3 = styled.h3`
+  font-size: 28px;
+  font-weight: bolder;
+  font-style: normal;
+  line-height: 38;
+  text-decoration: none;
+`;
+export const TypoH4 = styled.h4`
+  font-size: 24px;
+  font-weight: bolder;
+  font-style: normal;
+  line-height: 32;
+  text-decoration: none;
+`;
+
+export const TypoH5 = styled.h5`
+  font-size: 20px;
+  font-weight: bolder;
+  font-style: normal;
+  line-height: 28;
+  text-decoration: none;
+`;
+
+export const TypoH6 = styled.h6`
+  font-size: 16px;
+  font-weight: bolder;
+  font-style: normal;
+  line-height: 22;
+  text-decoration: none;
+`;
+
+export const TypoDisplay1 = styled.p`
+  font-size: 64px;
+  font-weight: bolder;
+  font-style: normal;
+  text-decoration: none;
+`;
+
+export const TypoDisplay2 = styled.p`
+  font-size: 56px;
+  font-weight: normal;
+  font-style: normal;
+  text-decoration: none;
+`;
+
+export const TypoCapitalized = styled.p`
+  font-size: 14px;
+  font-weight: normal;
+  font-style: normal;
+  letter-spacing: 2px;
+  text-decoration: none;
+`;
+
+export const TypoBlockQuote = styled.p`
+  font-size: 20px;
+  font-weight: bolder;
+  font-style: normal;
+  line-height: 28;
+  text-decoration: none;
+`;
+
+export const TypoStylizedLead = styled.p`
+  font-size: 14px;
+  font-weight: bolder;
+  font-style: normal;
+  line-height: 24;
+  text-decoration: none;
+`;
+
+export const TypoStylizedSmall = styled.p`
+  font-size: 12px;
+  font-weight: normal;
+  font-style: normal;
+  text-decoration: none;
+`;
+
+export const TypoStylizedTiny = styled.p`
+  font-size: 10px;
+  font-weight: bolder;
+  font-style: normal;
+  text-decoration: none;
+`;
