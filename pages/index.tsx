49956e063417b2d9305992e28c54cdd0b667eaf3
hoondeveloper <hoonskyn@cau.ac.kr>
2021-09-04 15:42:49 +0900

    refactor: 불필요한 코드 및 파일 삭제

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
-import { getSortedPostsData } from '../lib/posts';
 import Link from 'next/link';
-import Date from '../components/date';
 
 export default function Home({ allPostsData }) {
   return (
@@ -32,22 +30,10 @@ export default function Home({ allPostsData }) {
                 <a>{title}</a>
               </Link>
               <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
             </li>
           ))}
         </ul>
       </section>
     </Layout>
   );
-}
-
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData
-    }
-  };
 }
