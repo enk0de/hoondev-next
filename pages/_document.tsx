fd4fb9dcc9bd0f60122ac291ff22f4a7e2c0b543
hoondeveloper <hoonskyn@cau.ac.kr>
2021-09-04 21:26:17 +0900

    feat: Montserrat-Alternates 폰트 import

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -32,6 +32,10 @@ export default class MyDocument extends Document<MyDocumentProps> {
             type="text/css"
             href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css"
           />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
