b4207dcba4705efdf80951053229674b01f00a51
hoondeveloper <hoonskyn@cau.ac.kr>
2021-09-04 21:26:59 +0900

    feat: Layout에 스크롤 이벤트 리스너 추가

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,8 +1,24 @@
 import Footer from './Footer';
 import Header from './Header';
 import 'twin.macro';
+import { useEffect, useState } from 'react';
 
 export default function Layout({ children }) {
+  const [float, setFloat] = useState(false);
+  useEffect(() => {
+    const listener = () => {
+      if (window.scrollY !== 0) {
+        !float && setFloat(true);
+      } else {
+        float && setFloat(false);
+      }
+    };
+    window.addEventListener('scroll', listener);
+    return () => {
+      window.removeEventListener('scroll', listener);
+    };
+  });
+
   return (
     <>
       <Header />
