3b0e461ef240abfab3c1fdf4db24f9c4925ffb05
hoondeveloper <hoonskyn@cau.ac.kr>
2021-09-04 21:57:29 +0900

    fix: Layout에서 float 상태 업데이트 로직 수정
    스크롤 이벤트 리스너에 쓰로틀 적용

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -2,22 +2,25 @@ import Footer from './Footer';
 import Header from './Header';
 import 'twin.macro';
 import { useEffect, useState } from 'react';
+import { throttle } from 'lodash';
 
 export default function Layout({ children }) {
   const [float, setFloat] = useState(false);
+
   useEffect(() => {
-    const listener = () => {
+    const listener = throttle(() => {
       if (window.scrollY !== 0) {
         !float && setFloat(true);
       } else {
         float && setFloat(false);
       }
-    };
+    }, 300);
     window.addEventListener('scroll', listener);
+
     return () => {
       window.removeEventListener('scroll', listener);
     };
-  });
+  }, [float]);
 
   return (
     <>
