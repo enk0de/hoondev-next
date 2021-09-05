a402638f240b17157da52525a8a8354987a5443e
hoondeveloper <hoonskyn@cau.ac.kr>
2021-09-05 15:02:32 +0900

    feat: Footer 구현

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -1,3 +1,40 @@
+import Image from 'next/image';
+import 'twin.macro';
+import { TypoMediumLabel } from '../../styles/Typography';
+import { css } from '@emotion/react';
+import tw from 'twin.macro';
+
 export default function Footer() {
-  return <></>;
+  return (
+    <footer tw="px-[16px] py-[30px] mb-[50px] max-w-screen-lg mx-auto">
+      <div
+        tw="flex space-x-5"
+        css={css`
+          > a {
+            cursor: pointer;
+            ${tw`transition-opacity hover:opacity-75`}
+          }
+        `}
+      >
+        <a href="https://twitter.com/hoondeveloper" target="_blank" rel="noreferrer">
+          <Image src="/icons/twitter.svg" width={24} height={24} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/%EC%A7%80%ED%9B%88-%EC%9E%84-b441b81b5/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Image src="/icons/linkedin.svg" width={24} height={24} />
+        </a>
+        <a
+          href="https://www.rocketpunch.com/@hoonskyn9"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Image src="/icons/rocketpunch.svg" width={24} height={24} />
+        </a>
+      </div>
+      <TypoMediumLabel tw="text-dark-4">Frontend Engineer, Lim Ji Hoon</TypoMediumLabel>
+    </footer>
+  );
 }
