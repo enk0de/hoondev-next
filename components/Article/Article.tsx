430655660b5c49c2cf0d34b55e9e2434ee956004
hoondeveloper <hoonskyn@cau.ac.kr>
2021-09-05 15:02:59 +0900

    feat: Article 컴포넌트 고도화 및 스타일 수정

--- components/Article/Article.tsx
+++ components/Article/Article.tsx
@@ -1,20 +1,41 @@
 import React from 'react';
 import { TypoH3 } from '../../styles/Typography';
 import 'twin.macro';
+import tw, { css } from 'twin.macro';
+import { COLORS } from '../../shared/constants/colors';
 
 interface IDescriptionProps {
   title: string;
   description?: string;
+  smallDescription?: boolean;
 }
 
-const Article: React.FC<IDescriptionProps> = ({ title, description, children }) => {
+const Article: React.FC<IDescriptionProps> = ({
+  title,
+  description,
+  smallDescription,
+  children
+}) => {
   return (
     <article>
       <div>
         <TypoH3 tw="text-dark-1">{title}</TypoH3>
-        {description && <p tw="mt-[2px] text-dark-3">{description}</p>}
+        {description && (
+          <p
+            css={[
+              tw`mt-[6px] text-dark-2`,
+              smallDescription &&
+                css`
+                  color: ${COLORS.dark['3']};
+                  font-size: 14px;
+                `
+            ]}
+          >
+            {description}
+          </p>
+        )}
       </div>
-      {children}
+      <div tw="mt-[12px] text-dark-2">{children}</div>
     </article>
   );
 };
