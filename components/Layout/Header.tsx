1805e0253093bb404e0fddd2ad1fe961a16882aa
hoondeveloper <hoonskyn@cau.ac.kr>
2021-09-05 15:42:02 +0900

    feat: Header 반응형 대응

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -12,11 +12,13 @@ export default function Header({ float }: IHeaderProps) {
     <header tw="fixed top-0 left-0 right-0 m-[12px] max-lg:m-0">
       <div
         css={[
-          tw`flex max-w-screen-lg mx-auto space-x-6 bg-white px-[25px] py-[15px] transition-shadow rounded-2xl max-lg:rounded-none backdrop-filter backdrop-blur-2xl`,
+          tw`flex items-center max-w-screen-lg space-x-6 mx-auto bg-white px-[25px] py-[15px] transition-shadow rounded-2xl max-lg:rounded-none backdrop-filter backdrop-blur-2xl`,
           float && tw`shadow-light-el-4 bg-opacity-50`
         ]}
       >
-        <Image src="/images/logo.svg" width={172.5} height={24} alt="Logo" />
+        <div tw="w-[172.5px] h-[24px] relative max-sm:w-[137px] max-sm:h-[20px]">
+          <Image src="/images/logo.svg" priority layout="fill" alt="Logo" />
+        </div>
 
         <a href="https://feed.hoondev.com" target="_blank" rel="noreferrer">
           <StyledHeaderMenu>feed</StyledHeaderMenu>
@@ -32,5 +34,5 @@ export default function Header({ float }: IHeaderProps) {
 const StyledHeaderMenu = styled.button`
   font-family: 'Montserrat Alternates', sans-serif;
   font-size: 20px;
-  ${tw`text-dark-3 hover:text-dark-2 transition-colors`}
+  ${tw`text-dark-3 hover:text-dark-2 transition-colors max-sm:text-base`}
 `;
