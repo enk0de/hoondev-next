11e265a386076aa3f3f074e3b5eba0be843685f4
hoondeveloper <hoonskyn@cau.ac.kr>
2021-09-05 15:03:30 +0900

    feat: 학력 섹션 컴포넌트 구현

---
+++ components/Contents/Education.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import SectionFrame from '../Frame/SectionFrame';
+import Article from '../Article/Article';
+import 'twin.macro';
+import { css } from '@emotion/react';
+
+type DatasetType = {
+  title: string;
+  description: string;
+};
+
+const dataset: DatasetType[] = [
+  {
+    title: '총학점평균',
+    description: '3.97 / 4.50'
+  },
+  {
+    title: '전공학점평균',
+    description: '4.12 / 4.50'
+  }
+];
+
+export default function Education() {
+  return (
+    <SectionFrame title="학력">
+      <Article
+        title="중앙대학교 서울캠퍼스"
+        description="소프트웨어학부 (Computer Science and Engineering) 휴학"
+      >
+        <ul
+          css={css`
+            font-size: 14px;
+            line-height: 24px;
+          `}
+        >
+          {dataset.map((data, idx) => (
+            <li tw="space-x-2" key={idx}>
+              <b>{data.title}</b>
+              <span>{data.description}</span>
+            </li>
+          ))}
+        </ul>
+      </Article>
+    </SectionFrame>
+  );
+}
