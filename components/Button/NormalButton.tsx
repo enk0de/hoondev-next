b70ca05cf747e04a60aeef796f18432cff49175e
hoondeveloper <hoonskyn@cau.ac.kr>
2021-09-08 00:30:35 +0900

    feat: NormalButton 컴포넌트 구현

---
+++ components/Button/NormalButton.tsx
@@ -0,0 +1,15 @@
+import React, { ButtonHTMLAttributes } from 'react';
+import 'twin.macro';
+
+const NormalButton: React.FC<ButtonHTMLAttributes<any>> = ({ children, ...props }) => {
+  return (
+    <button
+      tw="inline-flex h-[38px] items-center text-dark-2 py-2 px-4 rounded-xl bg-light-2 hover:bg-light-1 transition-colors"
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default NormalButton;
