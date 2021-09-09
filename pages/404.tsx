de42cd95cf03801476aae9d066f030cdf4e12551
hoondeveloper <hoonskyn@cau.ac.kr>
2021-09-09 22:31:32 +0900

    feat: 404 페이지 추가

---
+++ pages/404.tsx
@@ -0,0 +1,18 @@
+/** @jsxImportSource @emotion/react */
+import Link from 'next/link';
+import 'twin.macro';
+import NormalButton from '../components/Button/NormalButton';
+
+function NotFound() {
+  return (
+    <div tw="px-[16px] space-y-4 h-screen flex flex-col items-center justify-center">
+      <h1 tw="max-sm:text-xl text-3xl font-bold text-dark-1">
+        존재하지 않거나 준비 중인 페이지입니다.
+      </h1>
+      <Link href="/" passHref={true}>
+        <NormalButton>메인으로</NormalButton>
+      </Link>
+    </div>
+  );
+}
+export default NotFound;
