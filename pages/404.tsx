c3ff094c249eb43e1ad18af3cb06c7793539b8f9
hoondeveloper <hoonskyn@cau.ac.kr>
2021-09-09 22:46:52 +0900

    fix: 404 페이지 스타일 수정

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,9 +5,11 @@ import NormalButton from '../components/Button/NormalButton';
 
 function NotFound() {
   return (
-    <div tw="px-[16px] space-y-4 h-screen flex flex-col items-center justify-center">
-      <h1 tw="max-sm:text-xl text-3xl font-bold text-dark-1">
-        존재하지 않거나 준비 중인 페이지입니다.
+    <div tw="px-[16px] space-y-4 mt-[200px] flex flex-col items-center justify-center">
+      <h1 tw="max-sm:text-xl text-3xl text-center font-bold text-dark-1">
+        존재하지 않거나
+        <br />
+        준비 중인 페이지입니다.
       </h1>
       <Link href="/" passHref={true}>
         <NormalButton>메인으로</NormalButton>
