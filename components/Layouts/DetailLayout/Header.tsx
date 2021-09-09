a5432289c0bbff399f83d5d7b325dd274ee4c39a
hoondeveloper <hoonskyn@cau.ac.kr>
2021-09-09 13:23:43 +0900

    feat: DetailLayout Header back button 구현

--- components/Layouts/DetailLayout/Header.tsx
+++ components/Layouts/DetailLayout/Header.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import 'twin.macro';
 import tw from 'twin.macro';
 import { TypoH6 } from '../../../styles/Typography';
+import { useRouter } from 'next/router';
 
 interface IHeaderProps {
   float?: boolean;
@@ -10,6 +11,7 @@ interface IHeaderProps {
 }
 
 export default function Header({ float, title }: IHeaderProps) {
+  const router = useRouter();
   return (
     <header tw="fixed z-50 top-0 left-0 right-0 m-[12px] max-lg:m-0">
       <div
@@ -18,9 +20,11 @@ export default function Header({ float, title }: IHeaderProps) {
           float && tw`shadow-light-el-4 bg-opacity-75`
         ]}
       >
-        <div tw="h-[30px] w-[50px] pl-[10px] flex">
-          <Image src="/icons/back.svg" priority width="24" height="24" />
-        </div>
+        <button onClick={() => router.back()}>
+          <div tw="h-[30px] w-[50px] pl-[10px] flex">
+            <Image src="/icons/back.svg" priority width="24" height="24" />
+          </div>
+        </button>
         <TypoH6 tw="text-dark-1">{title}</TypoH6>
       </div>
     </header>
