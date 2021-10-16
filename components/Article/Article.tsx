f887a8dc6a1c2a11f9f3a542e0930a765d678447
hoondeveloper <hoonskyn@cau.ac.kr>
2021-10-16 19:57:08 +0900

    fix: 불필요한 DOM depth 축소

--- components/Article/Article.tsx
+++ components/Article/Article.tsx
@@ -19,23 +19,21 @@ const Article: React.FC<IDescriptionProps> = ({
 }) => {
   return (
     <article>
-      <div>
-        <TypoH3 tw="text-dark-1 max-sm:text-xl">{title}</TypoH3>
-        {description && (
-          <p
-            css={[
-              tw`mt-[6px] text-dark-2`,
-              smallDescription &&
-                css`
-                  color: ${COLORS.dark['3']};
-                  font-size: 14px;
-                `
-            ]}
-          >
-            {description}
-          </p>
-        )}
-      </div>
+      <TypoH3 tw="text-dark-1 max-sm:text-xl">{title}</TypoH3>
+      {description && (
+        <p
+          css={[
+            tw`mt-[6px] text-dark-2`,
+            smallDescription &&
+              css`
+                color: ${COLORS.dark['3']};
+                font-size: 14px;
+              `
+          ]}
+        >
+          {description}
+        </p>
+      )}
       <div tw="mt-[12px] text-dark-2">{children}</div>
     </article>
   );
