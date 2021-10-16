7cdc29a14990e53ec96027f8246e6a7a3ba3ad1b
hoondeveloper <hoonskyn@cau.ac.kr>
2021-10-16 17:44:07 +0900

    fix: NormalButton forwardRef로 ref 주입받을 수 있도록 수정

--- components/Button/NormalButton.tsx
+++ components/Button/NormalButton.tsx
@@ -2,15 +2,21 @@
 import React, { ButtonHTMLAttributes } from 'react';
 import 'twin.macro';
 
-const NormalButton: React.FC<ButtonHTMLAttributes<any>> = ({ children, ...props }) => {
+const NormalButton = React.forwardRef<
+  HTMLButtonElement,
+  React.PropsWithChildren<ButtonHTMLAttributes<any>>
+>(({ children, ...props }, ref) => {
   return (
     <button
       tw="inline-flex h-[38px] items-center text-dark-2 py-2 px-4 rounded-xl bg-light-2 hover:bg-light-1 transition-colors"
+      ref={ref}
       {...props}
     >
       {children}
     </button>
   );
-};
+});
+
+NormalButton.displayName = 'NormalButton';
 
 export default NormalButton;
